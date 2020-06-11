4678b9500e46264bb6bd96bf33078304698daf87
hankhank10 <m.a.hankinson@gmail.com>
2020-06-11 16:34:14 +0100

    Update lastfm_user_data.py

--- lastfm_user_data.py
+++ lastfm_user_data.py
@@ -4,10 +4,7 @@ import json
 def check_data(lastfm_username, json_field):
     # build URL
     lastfm_public_key = "079a7d64ea52c358ad4f0afbe2f900b3"
-    
-    #lastfm_username = "markhank"
     url = "http://ws.audioscrobbler.com/2.0/?method=user.getinfo&user=" + lastfm_username + "&api_key=" + lastfm_public_key + "&format=json"
-    #print (url)
 
     # download the raw json object
     data = urllib.request.urlopen(url).read().decode()
